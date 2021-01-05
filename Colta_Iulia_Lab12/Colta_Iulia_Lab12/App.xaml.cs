64d77e54fcb1ed119511e816573d464e28a7d16d
IULIA MARIA COLTA <colta.iulia165249@student.ubbcluj.ro>
2021-01-05 10:42:17 +0200

    Add project files.

---
+++ Colta_Iulia_Lab12/Colta_Iulia_Lab12/App.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Colta_Iulia_Lab12.Services;
+using Colta_Iulia_Lab12.Views;
+using Colta_Iulia_Lab12.Data;
+
+namespace Colta_Iulia_Lab12
+{
+    public partial class App : Application
+    {
+        public static ShoppingListDatabase Database { get; private set; }
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+
+            Database = new ShoppingListDatabase(new RestService());
+            MainPage = new NavigationPage(new ListEntryPage());
+
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+
+    
+}
---
+++ Colta_Iulia_Lab12/Colta_Iulia_Lab12/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using Colta_Iulia_Lab12.ViewModels;
+
+namespace Colta_Iulia_Lab12.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
